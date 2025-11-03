377ed531388cbe068dfeb3ef5f62a3ee1d7d1d42
LailaSaudL <lailasaud98@gmail.com>
2025-11-03 13:11:37 +0100

    Update EditProfile.jsx

--- src/components/Main/components/Popup/form/NewCard/EditProfile/EditProfile.jsx
+++ src/components/Main/components/Popup/form/NewCard/EditProfile/EditProfile.jsx
@@ -1,34 +1,37 @@
-export default function NewCard() {
+export default function EditProfile() {
   return (
     <form
       className="popup__form"
-      name="card-form"
-      id="new-card-form"
+      name="edit-profile-form"
+      id="edit-profile-form"
       noValidate
     >
       <label className="popup__field">
         <input
-          className="popup__input popup__input_type_card-name"
-          id="card-name"
-          maxLength="30"
-          minLength="1"
-          name="card-name"
-          placeholder="Title"
+          className="popup__input popup__input_type_name"
+          id="name"
+          maxLength="40"
+          minLength="2"
+          name="name"
+          placeholder="Nombre"
           required
           type="text"
         />
-        <span className="popup__error" id="card-name-error"></span>
+        <span className="popup__error" id="name-error"></span>
       </label>
+
       <label className="popup__field">
         <input
-          className="popup__input popup__input_type_url"
-          id="card-link"
-          name="link"
-          placeholder="Image link"
+          className="popup__input popup__input_type_about"
+          id="about"
+          maxLength="200"
+          minLength="2"
+          name="about"
+          placeholder="Acerca de mí"
           required
-          type="url"
+          type="text"
         />
-        <span className="popup__error" id="card-link-error"></span>
+        <span className="popup__error" id="about-error"></span>
       </label>
 
       <button className="button popup__button" type="submit">
