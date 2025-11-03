7e62cb24c178997fdf98e119b516766a2a89eaf3
LailaSaudL <lailasaud98@gmail.com>
2025-11-03 13:08:10 +0100

    Update Main.js

--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -1,18 +1,67 @@
-import React from "react";
-import avatar from "../../images/avatar.jpg"; // ruta relativa
+import { useState } from "react";
+import Popup from "./components/Popup/Popup";
+import NewCard from "./components/form/NewCard/NewCard";
+import Card from "./components/Card/Card";
 
-function Main() {
+export default function Main() {
+  const [popup, setPopup] = useState(null);
+
+  const newCardPopup = { title: "Nuevo lugar", children: <NewCard /> };
+
+  const cards = [
+    {
+      isLiked: false,
+      _id: "5d1f0611d321eb4bdcd707dd",
+      name: "Yosemite Valley",
+      link: "https://practicum-content.s3.us-west-1.amazonaws.com/web-code/moved_yosemite.jpg",
+      owner: "5d1f0611d321eb4bdcd707dd",
+      createdAt: "2019-07-05T08:10:57.741Z",
+    },
+    {
+      isLiked: false,
+      _id: "5d1f064ed321eb4bdcd707de",
+      name: "Lake Louise",
+      link: "https://practicum-content.s3.us-west-1.amazonaws.com/web-code/moved_lake-louise.jpg",
+      owner: "5d1f0611d321eb4bdcd707dd",
+      createdAt: "2019-07-05T08:11:58.324Z",
+    },
+  ];
+
+  function handleOpenPopup(popup) {
+    setPopup(popup);
+  }
+
+  function handleClosePopup() {
+    setPopup(null);
+  }
+
   return (
     <main className="profile page__section">
-      <img
-        src={avatar}
-        alt="User avatar"
-        className="profile__avatar"
-      />
-      <h1 className="profile__name">User Name</h1>
-      <p className="profile__description">About user...</p>
+      <div className="profile__info">
+        <img
+          src="src/images/avatar.jpg"
+          alt="User avatar"
+          className="profile__avatar"
+        />
+        <button
+          aria-label="Add card"
+          className="profile__add-button"
+          type="button"
+          onClick={() => handleOpenPopup(newCardPopup)}
+        />
+      </div>
+
+      <ul className="cards__list">
+        {cards.map((card) => (
+          <Card key={card._id} card={card} handleOpenPopup={handleOpenPopup} />
+        ))}
+      </ul>
+
+      {popup && (
+        <Popup onClose={handleClosePopup} title={popup.title}>
+          {popup.children}
+        </Popup>
+      )}
     </main>
   );
 }
-
-export default Main;
