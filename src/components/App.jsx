57b0ee22aecc83bda02a68022b77f996aab98374
LailaSaudL <lailasaud98@gmail.com>
2025-11-03 11:57:36 +0100

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,1 +1,28 @@
+import React from "react";
+import logo from "./images/logo.svg";   // logo
+import avatar from "./images/avatar.jpg"; // avatar
+import "./App.css"; // estilos globales
 
+function App() {
+  return (
+    <div className="page__content">
+      <header className="header page__section">
+        <img
+          src={logo}
+          alt="Around the U.S logo"
+          className="logo header__logo"
+        />
+      </header>
+
+      <section className="profile page__section">
+        <img
+          src={avatar}
+          alt="User avatar"
+          className="profile__avatar"
+        />
+      </section>
+    </div>
+  );
+}
+
+export default App;
