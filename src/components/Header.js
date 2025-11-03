aedb7db55558c8223fa7064a69af20a34e9dca89
LailaSaudL <lailasaud98@gmail.com>
2025-11-03 12:02:16 +0100

    Update Header.js

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,1 +1,16 @@
+import React from "react";
+import logo from "../../images/logo.svg"; // ruta desde la carpeta Header
 
+function Header() {
+  return (
+    <header className="header page__section">
+      <img
+        src={logo}
+        alt="Around the U.S logo"
+        className="logo header__logo"
+      />
+    </header>
+  );
+}
+
+export default Header;
