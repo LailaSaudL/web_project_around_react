fbf25fbfe324fe18e6800225256cf0bed7b19207
LailaSaudL <lailasaud98@gmail.com>
2025-12-03 19:25:54 +0100

    Update EditAvatar.jsx

--- src/components/Main/components/Popup/form/NewCard/EditAvatar/EditAvatar.jsx
+++ src/components/Main/components/Popup/form/NewCard/EditAvatar/EditAvatar.jsx
@@ -1,10 +1,28 @@
+import { useRef, useContext } from "react";
+import CurrentUserContext from "../../../../../contexts/CurrentUserContext";
+
 export default function EditAvatar() {
+  // Obtener la función del contexto
+  const { handleUpdateAvatar } = useContext(CurrentUserContext);
+
+  // Ref para acceder al input directamente
+  const avatarInputRef = useRef();
+
+  // Handler para enviar el formulario
+  function handleSubmit(event) {
+    event.preventDefault();
+    handleUpdateAvatar({
+      avatar: avatarInputRef.current.value,
+    });
+  }
+
   return (
     <form
       className="popup__form"
       name="edit-avatar-form"
       id="edit-avatar-form"
       noValidate
+      onSubmit={handleSubmit}
     >
       <label className="popup__field">
         <input
@@ -14,9 +32,16 @@ export default function EditAvatar() {
           placeholder="Enlace de avatar"
           required
           type="url"
+          ref={avatarInputRef}
         />
         <span className="popup__error" id="avatar-url-error"></span>
       </label>
+      <button className="button popup__button" type="submit">
+        Guardar
+      </button>
+    </form>
+  );
+}
 
       <button className="button popup__button" type="submit">
         Guardar
