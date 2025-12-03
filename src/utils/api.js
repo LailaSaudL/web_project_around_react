27bd8e0286efa6394eddd092cb57cbf0d2d21435
LailaSaudL <lailasaud98@gmail.com>
2025-12-03 14:35:01 +0100

    Update api.js

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,1 +1,79 @@
+class Api {
+  constructor({ baseUrl, headers }) {
+    this._baseUrl = baseUrl;
+    this._headers = headers;
+  }
 
+  _checkRes(res) {
+    if (res.ok) return res.json();
+    return Promise.reject(`Error: ${res.status}`);
+  }
+
+  // Obtener información del usuario
+  getUserInfo() {
+    return fetch(`${this._baseUrl}/users/me`, {
+      headers: this._headers,
+    }).then(this._checkRes);
+  }
+
+  // Obtener tarjetas
+  getInitialCards() {
+    return fetch(`${this._baseUrl}/cards`, {
+      headers: this._headers,
+    }).then(this._checkRes);
+  }
+
+  // Editar perfil
+  setUserInfo({ name, about }) {
+    return fetch(`${this._baseUrl}/users/me`, {
+      method: "PATCH",
+      headers: this._headers,
+      body: JSON.stringify({ name, about }),
+    }).then(this._checkRes);
+  }
+
+  // Agregar nueva tarjeta
+  addCard({ name, link }) {
+    return fetch(`${this._baseUrl}/cards`, {
+      method: "POST",
+      headers: this._headers,
+      body: JSON.stringify({ name, link }),
+    }).then(this._checkRes);
+  }
+
+  // Eliminar tarjeta
+  deleteCard(cardId) {
+    return fetch(`${this._baseUrl}/cards/${cardId}`, {
+      method: "DELETE",
+      headers: this._headers,
+    }).then(this._checkRes);
+  }
+
+  // Cambiar estado de like (dar o quitar)
+  changeLikeCardStatus(cardId, isLiked) {
+    return fetch(`${this._baseUrl}/cards/${cardId}/likes`, {
+      method: isLiked ? "PUT" : "DELETE",
+      headers: this._headers,
+    }).then(this._checkRes);
+  }
+
+  // Cambiar avatar
+  setUserAvatar({ avatar }) {
+    return fetch(`${this._baseUrl}/users/me/avatar`, {
+      method: "PATCH",
+      headers: this._headers,
+      body: JSON.stringify({ avatar }),
+    }).then(this._checkRes);
+  }
+}
+
+// Exportamos la instancia (no la clase)
+const api = new Api({
+  baseUrl: "https://around-api.es.tripleten-services.com/v1",
+  headers: {
+    authorization: "327f677d-2fa5-4635-8f0a-94301860a124",
+    "Content-Type": "application/json",
+  },
+});
+
+export default api;
