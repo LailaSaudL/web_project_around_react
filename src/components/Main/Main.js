2b9af6d926e51100a9b402135be51d45d6739d3b
LailaSaudL <lailasaud98@gmail.com>
2025-12-03 19:21:48 +0100

    Update Main.js

--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -1,64 +1,72 @@
-import { useState } from "react";
+import { useContext } from "react";
+import CurrentUserContext from "../../contexts/CurrentUserContext";
 import Popup from "./components/Popup/Popup";
-import NewCard from "./components/form/NewCard/NewCard";
 import Card from "./components/Card/Card";
+import NewCard from "./components/form/NewCard/NewCard";
+import EditProfile from "./components/form/NewCard/EditProfile/EditProfile";
+import EditAvatar from "./components/form/NewCard/EditAvatar/EditAvatar";
 
-export default function Main() {
-  const [popup, setPopup] = useState(null);
+export default function Main({
+  cards,
+  popup,
+  onOpenPopup,
+  onClosePopup,
+  onCardLike,
+  onCardDelete,
+}) {
+  // Obtener datos del usuario desde el contexto
+  const { currentUser } = useContext(CurrentUserContext);
 
+  // Configuración de los popups
   const newCardPopup = { title: "Nuevo lugar", children: <NewCard /> };
-
-  const cards = [
-    {
-      isLiked: false,
-      _id: "5d1f0611d321eb4bdcd707dd",
-      name: "Yosemite Valley",
-      link: "https://practicum-content.s3.us-west-1.amazonaws.com/web-code/moved_yosemite.jpg",
-      owner: "5d1f0611d321eb4bdcd707dd",
-      createdAt: "2019-07-05T08:10:57.741Z",
-    },
-    {
-      isLiked: false,
-      _id: "5d1f064ed321eb4bdcd707de",
-      name: "Lake Louise",
-      link: "https://practicum-content.s3.us-west-1.amazonaws.com/web-code/moved_lake-louise.jpg",
-      owner: "5d1f0611d321eb4bdcd707dd",
-      createdAt: "2019-07-05T08:11:58.324Z",
-    },
-  ];
-
-  function handleOpenPopup(popup) {
-    setPopup(popup);
-  }
-
-  function handleClosePopup() {
-    setPopup(null);
-  }
+  const editProfilePopup = { title: "Editar perfil", children: <EditProfile /> };
+  const editAvatarPopup = { title: "Cambiar foto de perfil", children: <EditAvatar /> };
 
   return (
-    <main className="profile page__section">
-      <div className="profile__info">
-        <img
-          src="src/images/avatar.jpg"
-          alt="User avatar"
-          className="profile__avatar"
-        />
+    <main className="content">
+      <section className="profile page__section">
+        <div className="profile__image-container">
+          <img
+            src={currentUser.avatar}
+            alt="User avatar"
+            className="profile__avatar"
+            onClick={() => onOpenPopup(editAvatarPopup)}
+          />
+        </div>
+        <div className="profile__info">
+          <h1 className="profile__name">{currentUser.name}</h1>
+          <button
+            aria-label="Edit profile"
+            className="profile__edit-button"
+            type="button"
+            onClick={() => onOpenPopup(editProfilePopup)}
+          />
+          <p className="profile__about">{currentUser.about}</p>
+        </div>
         <button
           aria-label="Add card"
           className="profile__add-button"
           type="button"
-          onClick={() => handleOpenPopup(newCardPopup)}
+          onClick={() => onOpenPopup(newCardPopup)}
         />
-      </div>
+      </section>
 
-      <ul className="cards__list">
-        {cards.map((card) => (
-          <Card key={card._id} card={card} handleOpenPopup={handleOpenPopup} />
-        ))}
-      </ul>
+      <section className="cards page__section">
+        <ul className="cards__list">
+          {cards.map((card) => (
+            <Card
+              key={card._id}
+              card={card}
+              onCardLike={onCardLike}
+              onCardDelete={onCardDelete}
+              onOpenPopup={onOpenPopup}
+            />
+          ))}
+        </ul>
+      </section>
 
       {popup && (
-        <Popup onClose={handleClosePopup} title={popup.title}>
+        <Popup onClose={onClosePopup} title={popup.title}>
           {popup.children}
         </Popup>
       )}
